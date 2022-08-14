16ce61097f1f89c5c65dc04736c9f8828121aa63
asuyasuya <asuya0222@gmail.com>
2022-08-14 16:40:19 +0900

    update: ログインページ

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter,Routes, Route} from 'react-router-dom';
 import Users from "./pages/Users";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -9,9 +9,11 @@ function App() {
     return (
         <div className="App">
             <BrowserRouter>
-                <Route path={'/'} exact component={Users}/>
-                <Route path={'/login'} component={Login}/>
-                <Route path={'/register'} component={Register}/>
+                <Routes>
+                    <Route path={'/'} element={<Users/>}/>
+                    <Route path="/login" element={<Login />} />
+                    <Route path={'/register'} element={<Register />}/>
+                </Routes>
             </BrowserRouter>
         </div>
     );
--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import Menu from "../componets/Menu";
 
 const Users = () => {
     return (
         <div>
-            
+            <div className="container-fluid">
+                <div className="row">
+                    <Menu/>
+                    <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                        <div className="table-responsive">
+                            <table className="table table-striped table-sm">
+                                <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Header</th>
+                                    <th>Header</th>
+                                    <th>Header</th>
+                                    <th>Header</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <td>1,001</td>
+                                    <td>random</td>
+                                    <td>data</td>
+                                    <td>placeholder</td>
+                                    <td>text</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </main>
+                </div>
+            </div>
         </div>
     );
 };
