d1e228607aedab77092250dc2c15e4ea31a8c413
asuyasuya <asuya0222@gmail.com>
2022-08-18 23:26:02 +0900

    update: 認証済みユーザでないとログイン画面に戻すように変更

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,38 +1,31 @@
 import React from 'react';
-import Menu from "../componets/Menu";
+import Layout from "../componets/Layout";
 
 const Users = () => {
     return (
         <div>
-            <div className="container-fluid">
-                <div className="row">
-                    <Menu/>
-                    <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                        <div className="table-responsive">
-                            <table className="table table-striped table-sm">
-                                <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Header</th>
-                                    <th>Header</th>
-                                    <th>Header</th>
-                                    <th>Header</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr>
-                                    <td>1,001</td>
-                                    <td>random</td>
-                                    <td>data</td>
-                                    <td>placeholder</td>
-                                    <td>text</td>
-                                </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </main>
-                </div>
-            </div>
+            <Layout>
+                <table className="table table-striped table-sm">
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Header</th>
+                        <th>Header</th>
+                        <th>Header</th>
+                        <th>Header</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>1,001</td>
+                        <td>random</td>
+                        <td>data</td>
+                        <td>placeholder</td>
+                        <td>text</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </Layout>
         </div>
     );
 };
