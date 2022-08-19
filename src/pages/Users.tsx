8aafb6df6a6859ba4dedc58c8f902fcf24b02af5
asuyasuya <asuya0222@gmail.com>
2022-08-20 01:43:42 +0900

    update: ユーザー一覧の表示

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Layout from "../componets/Layout";
+import axios from "axios";
+import {User} from "../models/user";
 
 const Users = () => {
+    const [users, setUsers] = useState<User[]>([]);
+
+    useEffect(() => {
+        (
+            async () => {
+                const {data} = await axios.get('ambassadors')
+                console.log(data);
+
+                setUsers(data);
+            }
+        )()
+    },[])
     return (
         <div>
             <Layout>
@@ -9,20 +23,22 @@ const Users = () => {
                     <thead>
                     <tr>
                         <th>#</th>
-                        <th>Header</th>
-                        <th>Header</th>
-                        <th>Header</th>
-                        <th>Header</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Actions</th>
                     </tr>
                     </thead>
                     <tbody>
-                    <tr>
-                        <td>1,001</td>
-                        <td>random</td>
-                        <td>data</td>
-                        <td>placeholder</td>
-                        <td>text</td>
-                    </tr>
+                    {users.map(user => {
+                        return (
+                            <tr>
+                                <td>{user.id}</td>
+                                <td>{user.first_name} {user.last_name}</td>
+                                <td>{user.email}</td>
+                                <td></td>
+                            </tr>
+                        )
+                    })}
                     </tbody>
                 </table>
             </Layout>
