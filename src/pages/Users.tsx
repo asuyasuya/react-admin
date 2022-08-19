2822337f5109fecef6de151cb933c5c9c836ca59
asuyasuya <asuya0222@gmail.com>
2022-08-20 02:04:43 +0900

    update: material_uiの導入

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import Layout from "../componets/Layout";
 import axios from "axios";
 import {User} from "../models/user";
+import {Table, TableBody, TableCell, TableHead, TableRow} from "@mui/material";
 
 const Users = () => {
     const [users, setUsers] = useState<User[]>([]);
@@ -19,28 +20,28 @@ const Users = () => {
     return (
         <div>
             <Layout>
-                <table className="table table-striped table-sm">
-                    <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Actions</th>
-                    </tr>
-                    </thead>
-                    <tbody>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>#</TableCell>
+                            <TableCell>Name</TableCell>
+                            <TableCell>Email</TableCell>
+                            <TableCell>Actions</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
                     {users.map(user => {
                         return (
-                            <tr>
-                                <td>{user.id}</td>
-                                <td>{user.first_name} {user.last_name}</td>
-                                <td>{user.email}</td>
-                                <td></td>
-                            </tr>
+                            <TableRow key={user.id}>
+                                <TableCell>{user.id}</TableCell>
+                                <TableCell>{user.first_name} {user.last_name}</TableCell>
+                                <TableCell>{user.email}</TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
                         )
                     })}
-                    </tbody>
-                </table>
+                    </TableBody>
+                </Table>
             </Layout>
         </div>
     );
