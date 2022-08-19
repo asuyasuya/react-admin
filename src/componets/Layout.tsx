d50265450af6a3f1dae6acb498a445d989ffd17f
asuyasuya <asuya0222@gmail.com>
2022-08-19 12:40:18 +0900

    update: ログアウト機能の実装

--- src/componets/Layout.tsx
+++ src/componets/Layout.tsx
@@ -3,14 +3,17 @@ import Nav from "./Nav";
 import Menu from "./Menu";
 import axios from "axios";
 import {Navigate} from "react-router-dom";
+import {User} from "../models/user";
 
 const Layout = (props: any) => {
     const [redirect, setRedirect] = useState(false);
+    const [user, setUser] = useState<User | null>(null);
+
     useEffect(() => {
         (
             async () => {
                 try {
-                    const {data} = await axios.get('user', );
+                    const {data} = await axios.get('user',);
 
                     console.log(data);
                 } catch (e) {
@@ -27,7 +30,7 @@ const Layout = (props: any) => {
 
     return (
         <div>
-            <Nav/>
+            <Nav user={user}/>
             <div className="container-fluid">
                 <div className="row">
                     <Menu/>
--- src/componets/Nav.tsx
+++ src/componets/Nav.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import {User} from "../models/user";
+import {Link} from "react-router-dom";
+import axios from "axios";
 
-const Nav = () => {
+const Nav = (props: { user: User | null }) => {
     return (
         <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
             <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Company name</a>
             <ul className="navbar-nav px-3">
-                <li className="nav-item text-nowrap">
-                    <a className="nav-link" href="#">Sign out</a>
-                </li>
+                <Link to={'/profile'}
+                      className="p-2 text-white text-decoration-none">{props.user?.first_name} {props.user?.last_name}</Link>
+                <Link to={'/login'}
+                      className="p-2 text-white text-decoration-none"
+                      onClick={async () => await axios.post('logout')}
+                >Sign out</Link>
             </ul>
         </header>
     );
